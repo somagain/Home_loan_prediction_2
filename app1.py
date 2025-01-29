e5ae4be220cfa26f24a727feedde5352bbbc1518
somagain <44696775+somagain@users.noreply.github.com>
2025-01-29 09:45:46 +0100

    update

--- app1.py
+++ app1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.preprocessing import StandardScaler, OrdinalEncoder, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
